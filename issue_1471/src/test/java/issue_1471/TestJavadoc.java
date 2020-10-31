cc2595549be84a5cae1d1f711f415b11a63dbcf2
gitmotte <www@synbee.at>
2020-10-31 16:47:09 +0100

    https://github.com/eclipse-ee4j/jaxb-ri/issues/1471
    failing testcase

---
+++ issue_1471/src/test/java/issue_1471/TestJavadoc.java
@@ -0,0 +1,50 @@
+package issue_1471;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import com.sun.codemodel.ClassType;
+import com.sun.codemodel.JClassAlreadyExistsException;
+import com.sun.codemodel.JCodeModel;
+import com.sun.codemodel.JDefinedClass;
+import com.sun.codemodel.JDocComment;
+import com.sun.codemodel.writer.OutputStreamCodeWriter;
+import org.junit.jupiter.api.Test;
+
+public class TestJavadoc {
+
+    @Test
+    public void testJavadoc() throws JClassAlreadyExistsException, IOException {
+        JCodeModel model = new JCodeModel();
+        String className = "issue_1471.JavadocTest";
+        JDefinedClass cls = model._class(className, ClassType.CLASS);
+        JDocComment comment = cls.javadoc();
+        comment.add("<b>a bold text</b><br>\n");
+        comment.add("<p>a text within paraphrases</p>");
+
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        OutputStreamCodeWriter fileCodeWriter = new OutputStreamCodeWriter(os, "UTF-8");
+        model.build(fileCodeWriter);
+
+        String generatedClass = os.toString(StandardCharsets.UTF_8);
+        assertEquals("\n"
+                + "package issue_1471;\n"
+                + "\n"
+                + "\n"
+                + "\n"
+                + "/**\n"
+                + " * <b>a bold text</b><br>\n"
+                + " * <p>a text within paraphrases</p>\n"
+                + " * \n"
+                + " */\n"
+                + "public class JavadocTest {\n"
+                + "\n"
+                + "\n"
+                + "}\n"
+                + "", generatedClass);
+    }
+
+}
